c0b11a86b82b5f32481d5c97a234ec7203589bf9
1352348805 <1352348805@qq.com>
2019-08-23 20:23:05 +0800

    测试文件

--- test/TestDemo.java
+++ test/TestDemo.java
@@ -1,13 +1,42 @@
 import area.Circle;
+import classDemo.Animal;
+import classDemo.Cat;
+import classDemo.People;
+import org.junit.Test;
+import page88x5.DotMatrixPrinter;
+import page88x5.InkpetPrinter;
+import page88x5.LaserPrinter;
+import page88x5.Printer;
+import page89x6.Friends;
+import page89x6.Niu;
+import page89x6.Wang;
 
 public class TestDemo {
 
-    public static void main(String[] args) {
-        Circle c = new Circle();
+    @Test
+    public void t1() {
+        Animal cat = new Cat("莉莉");
+        cat.setHealth(50);
+        People p = new People("a");
+        p.getFood(cat);
+    }
 
-        c.setWide(10);
-        double area = c.GraphArea();
-        System.out.println("面积是：" +area);
+    @Test
+    public void t2() {
+        Printer dotMatrixPrinter = new DotMatrixPrinter();
+        Printer inkpetPrinter = new InkpetPrinter();
+        Printer laserPrinter = new LaserPrinter();
+        dotMatrixPrinter.print();
+        inkpetPrinter.print();
+        laserPrinter.print();
+    }
+
+    @Test
+    public void t3() {
+
+        Friends wang = new Wang();
+        Niu niu = new Niu();
+        niu.serveFriend(wang);
 
     }
 }
