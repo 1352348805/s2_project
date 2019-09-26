821e282e00c834c7b89647f9544f8e3aa9130a2a
1352348805 <1352348805@qq.com>
2019-09-26 21:51:49 +0800

    第六章作业

--- unit6/AnimalShop/TestDemo.java
+++ unit6/AnimalShop/TestDemo.java
@@ -4,17 +4,19 @@ import java.util.*;
 
 public class TestDemo {
     public static void main(String[] args) {
-        List<Dog> list = new ArrayList<Dog>();
+        Map<String,Dog> map = new HashMap<String,Dog>();
         Dog dog1 = new Dog("小黄","拉布拉多");
         Dog dog2 = new Dog("小黑","哈士奇");
         Dog dog3 = new Dog("小白","拉布拉多");
-        list.add(dog1);
-        list.add(dog2);
-        list.add(dog3);
-        Iterator i = list.iterator();
-        while(i.hasNext()){
-            Dog d = (Dog)i.next();
-            System.out.println(d.getName()+"    "+d.getBrand());
+        map.put(dog1.getName(),dog1);
+        map.put(dog2.getName(),dog2);
+        map.put(dog3.getName(),dog3);
+        Set set = map.keySet();
+        Iterator iterator = set.iterator();
+        while(iterator.hasNext()){
+            String key = (String)iterator.next();
+            Dog dog = (Dog)map.get(key);
+            System.out.println(dog.getName()+"    "+dog.getBrand());
         }
     }
 }
